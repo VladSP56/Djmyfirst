af9e322b3bd5addea9578bd9a73f8abee863b73c
VladSP56 <parkvls@gmail.com>
2025-04-08 18:27:25 +0300

    add files

--- myfirstdjango/myfdjapp/views.py
+++ myfirstdjango/myfdjapp/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+#def data_view(request):
+   # return HttpResponse("<h1> This is page of Data</h1><p>Any content can be here.</p>")
+
+#def test_view(request):
+   # return HttpResponse("<h1> This is page of Test</h1><p> Another unic content!</p>")
+from django.shortcuts import render
 
 def data_view(request):
-    return HttpResponse("<h1> This is page of Data</h1><p>Any content can be here.</p>")
+    return render(request, 'myfdjapp/data.html')
 
 def test_view(request):
-    return HttpResponse("<h1> This is page of Test</h1><p> Another unic content!</p>")
+    return render(request, 'myfdjapp/test.html')
